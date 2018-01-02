76886c396cfecfe78c5c2af10d27903a0233ef8d
Filip Bech-Larsen <filipbech@filipbech.dk>
2018-01-02 21:15:28 +0100

    relative protocol on api calls

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 export const getTournaments = () => {
-    return fetch(`http://foosrank.azurewebsites.net/api/tournament/GetTournaments`).then(res=>res.json());
+    return fetch(`//foosrank.azurewebsites.net/api/tournament/GetTournaments`).then(res=>res.json());
 }
 
 export const getTournament = id => {
-    return fetch(`http://foosrank.azurewebsites.net/api/tournament/GetTournament?id=${id}`).then(res => res.json());
+    return fetch(`//foosrank.azurewebsites.net/api/tournament/GetTournament?id=${id}`).then(res => res.json());
 }
 
 let usersRequest;
@@ -13,21 +13,21 @@ export const getAllUsers = () => {
         return usersRequest;
     } 
     // add caching here...
-    usersRequest = fetch(`http://foosrank.azurewebsites.net/api/User/GetAllUsers`).then(res => res.json());
+    usersRequest = fetch(`//foosrank.azurewebsites.net/api/User/GetAllUsers`).then(res => res.json());
     return usersRequest;
 }
 
 export const getUserStatistics = (username, tournamentId) => {
-    return fetch(`http://foosrank.azurewebsites.net/api/Statistics/GetUserStatistics?tournamentId=${tournamentId}&username=${username}`).then(res => res.json());
+    return fetch(`//foosrank.azurewebsites.net/api/Statistics/GetUserStatistics?tournamentId=${tournamentId}&username=${username}`).then(res => res.json());
 }
 
 export const getTournamentUserRanks = tournamentId => {
-    return fetch(`http://foosrank.azurewebsites.net/api/Statistics/GetTournamentUserRanks?tournamentId=${tournamentId}`).then(res => res.json());
+    return fetch(`//foosrank.azurewebsites.net/api/Statistics/GetTournamentUserRanks?tournamentId=${tournamentId}`).then(res => res.json());
 }
 
 export const createMatch = (tournamentId = '', match = {}) => {
     match['TournamentId'] = tournamentId;
-    return fetch(`http://foosrank.azurewebsites.net/api/match/creatematch`, {
+    return fetch(`//foosrank.azurewebsites.net/api/match/creatematch`, {
         method: 'post',
         body: JSON.stringify(match),
         headers: { 'Content-Type': 'application/json' }
